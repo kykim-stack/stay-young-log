2ad0e40f2b70b1966cc8fa16b7df21174112cc86
kikayoung <kayoung7134@gmail.com>
2025-12-29 14:08:54 +0900

    feat: 썸네일 추가

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -21,14 +21,26 @@ export async function getAllPosts() {
       },
       sorts: [{ property: 'Date', direction: 'descending' }],
     });
+    return response.results.map((page: any) => {
+      const thumbnailProperty = page.properties.Thumbnail?.files?.[0];
+      let thumbnail = '';
 
-    return response.results.map((page: any) => ({
-      slug: page.properties.Slug?.rich_text[0]?.plain_text || '',
-      title: page.properties.Name?.title[0]?.plain_text || '',
-      date: page.properties.Date?.date?.start || '',
-      category: page.properties.Category?.select?.name || '전체',
-      description: page.properties.Description?.rich_text[0]?.plain_text || '',
-    }));
+      if (thumbnailProperty) {
+        // 직접 업로드한 파일인 경우 vs 외부 링크인 경우
+        thumbnail =
+          thumbnailProperty.file?.url || thumbnailProperty.external?.url || '';
+      }
+
+      return {
+        slug: page.properties.Slug?.rich_text[0]?.plain_text || '',
+        title: page.properties.Name?.title[0]?.plain_text || '',
+        date: page.properties.Date?.date?.start || '',
+        category: page.properties.Category?.select?.name || '전체',
+        description:
+          page.properties.Description?.rich_text[0]?.plain_text || '',
+        thumbnail: thumbnail,
+      };
+    });
   } catch (error) {
     console.error('노션 글 불러오기 에러:', error);
     return [];
@@ -51,12 +63,16 @@ export async function getPostData(slug: string) {
     const mdblocks = await n2m.pageToMarkdown(page.id);
     const mdString = n2m.toMarkdownString(mdblocks);
 
+    const thumbnailProperty = page.properties.Thumbnail?.files?.[0];
+    const thumbnail =
+      thumbnailProperty?.file?.url || thumbnailProperty?.external?.url || '';
     return {
       title: page.properties.Name?.title[0]?.plain_text,
       date: page.properties.Date?.date?.start,
       content: mdString.parent,
       category: page.properties.Category?.select?.name,
       description: page.properties.Description?.rich_text[0]?.plain_text || '',
+      thumbnail: thumbnail,
     };
   } catch (error) {
     console.error('본문 불러오기 실패:', error);
